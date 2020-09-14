60a8386e9f6082a1666ccb5bc9de15bca569bf44
d-ando <moruze.orange@gmail.com>
2020-09-15 08:54:02 +0900

    add hello route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello world!';
+});
+
+Route::get('/hello/{name}', function ($name) {
+    return 'Hello '.$name.'!';
+});
